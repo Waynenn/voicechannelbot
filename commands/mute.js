3baa62c9556cb206e9ba0b21f5aae12f1217b455
enderman143 <43355709+outlaylol@users.noreply.github.com>
2019-10-31 07:51:52 -0600

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -4,8 +4,7 @@
 
 
    
-    let member = msg.mentions.members.first()
-    if (!msg.mentions.members.first()) return msg.channel.send('woopsi i forgot to mention a user').then(m => m.delete())
+    let member = msg.mentions.members.first() || msg.guild.members.find(mem => mem.user.username == args[0]);
     member.setDeaf(true, "idiot")
     return;
 }
