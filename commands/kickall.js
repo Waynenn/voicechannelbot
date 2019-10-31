4652b096d9cd828380a2bc0b74daadd5517c2820
enderman143 <43355709+outlaylol@users.noreply.github.com>
2019-10-31 07:50:23 -0600

    Update kickall.js

--- commands/kickall.js
+++ commands/kickall.js
@@ -1,12 +1,20 @@
 module.exports = (client, msg, args) => {
 
- let array = msg.guild.members.array()
+let array = msg.guild.channels.array().filter(v => v.type == "voice")
 
-    for (var i = 0; i < array.length; i++) {
-        console.log('hello  ')
-        array[i].setVoiceChannel(null)
-        
+for (var i = 0; i < array.length; i++) {
+    let array2 = []
+    array2.push(array[i].id)
+    for (var i = 0; i < array2.length; i++) {
+        let members = client.channels.get(array2[i]).members.array()
+        for(var i = 0; i < members.length; i++) {
+            members[i].setVoiceChannel(null)
+        }
+
     }
+}
+    array2 = [];
     return;
+
 
 }
